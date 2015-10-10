c17071b93d42ccf24fc8a4a84816334334e1be0a
Daniel <daniel@ubuntu.(none)>
2015-10-10 12:12:20 +0200

    malloc sanity test successful

--- mm_alloc.c
+++ mm_alloc.c
@@ -6,35 +6,123 @@
  */
 
 #include "mm_alloc.h"
-
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <assert.h>
 
 /* Your final implementation should comment out this macro. */
-#define MM_USE_STUBS
+//#define MM_USE_STUBS
+
+s_block_ptr start = NULL;
+s_block_ptr end = NULL;
+
+
+s_block_ptr extend_heap(s_block_ptr last, size_t s){
+	s_block_ptr new = (s_block_ptr)sbrk(BLOCK_SIZE + s);
+	if (new == NULL) return NULL;
+	
+	if (start == NULL) {
+		s_block_ptr dummy = (s_block_ptr)sbrk(BLOCK_SIZE);
+		dummy->size = (size_t)0;
+		dummy->next = NULL;
+		dummy->prev = NULL;
+		dummy->free = 0;
+		dummy->ptr = NULL;
+		start = dummy;
+		end = start;
+	}
+	
+	end->next = new;
+	new->prev = end;
+	new->next = NULL;
+	new->size = s;
+	new->ptr = new->data;
+	new->free = 1;
+	end = new;
+	return new;
+}
+
+s_block_ptr fusion(s_block_ptr b){
+	//Fuse block b with next and previous if they exist and are free
+	s_block_ptr curr = b;
+	int count;
+	int wentLeft = 0;
+	int wentRight = 0;
+	//Forward
+	count = 0;
+	while ( (curr->next != NULL) && (curr->next->free) ){
+		wentRight = 1;
+		count += (curr->next->size + BLOCK_SIZE);
+		curr = curr->next;
+	}
+	
+	if (wentRight){
+		b->size += count;
+		b->next = curr->next;
+		curr->next->prev = b;
+	}
+
+	//Backward
+	count = 0;
+	while ( (curr->prev != NULL) && (curr->prev->free) ){
+		wentLeft = 1;
+		count += (curr->prev->size + BLOCK_SIZE);
+		curr = curr->prev;
+	}
+	
+	if (wentLeft){
+		curr->prev->size += count;
+		curr->prev->next = b->next;
+		b->next->prev = curr->prev;
+		return curr->prev;
+	}
+	
+	return b;
+}
+
+void split_block(s_block_ptr b, size_t s){
+	s_block_ptr new = (s_block_ptr)(b->next-(BLOCK_SIZE + s));
+	new->size = s;
+	new->next = b->next;
+	new->prev = b;
+	new->free = 1;
+	new->ptr = new->data;
+	b->size -= (s+BLOCK_SIZE);
+	b->next = new;
+	
+	if (new->next != NULL) new->next->prev = new;
+}
+
+s_block_ptr get_block(void *p){
+	return p -= BLOCK_SIZE;
+}
 
 void* mm_malloc(size_t size)
-{
-#ifdef MM_USE_STUBS
-    return calloc(1, size);
-#else
-#error Not implemented.
-#endif
+{	
+	s_block_ptr curr = start;
+	while ( (curr != NULL)&&((curr->free == 0)||(curr->size < size)) ){
+		curr = curr->next;
+	}
+	
+	if (curr == NULL){
+		s_block_ptr new = extend_heap(end, size);
+		new->free = 0;
+		return new->ptr;
+	}
+	else return (curr->ptr);
 }
 
 void* mm_realloc(void* ptr, size_t size)
 {
-#ifdef MM_USE_STUBS
-    return realloc(ptr, size);
-#else
-#error Not implemented.
-#endif
+	void* result = mm_malloc(size);
+	memcpy(result, ptr, size);
+	return result;
 }
 
 void mm_free(void* ptr)
 {
-#ifdef MM_USE_STUBS
-    free(ptr);
-#else
-#error Not implemented.
-#endif
+	s_block_ptr curr = get_block(ptr);
+	curr->free = 1;
+	fusion(curr);
 }
--- mm_test.c
+++ mm_test.c
@@ -4,11 +4,12 @@
 #include <stdio.h>
 int main(int argc, char **argv)
 {
-    int *data;
-
-    data = (int*) mm_malloc(4);
-    data[0] = 1;
-    mm_free(data);
-    printf("malloc sanity test successful!\n");
-    return 0;
+	int *data;
+	
+	data = (int*) mm_malloc(4);
+	data[0] = 1;
+	
+	mm_free(data);
+	printf("malloc sanity test successful!\n");
+	return 0;
 }
